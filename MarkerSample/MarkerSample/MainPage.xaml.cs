a8aaec5dfccf3178b50f17b46f78dcbf969fcd59
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-08-19 18:19:25 +0530

    The sample included

---
+++ MarkerSample/MarkerSample/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Syncfusion.SfMaps.XForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MarkerSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Layer_MarkerSelected(object sender, MarkerSelectedEventArgs e)
+        {
+            e.CustomView = this.Resources["selectedMarker"] as DataTemplate;
+        }
+
+    }
+}
